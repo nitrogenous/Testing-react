2d65b5894045ad48dd9f0170abf058a0d04a2af6
Toprak Koç <toprakademkoc360@gmail.com>
2018-02-07 19:17:33 +0300

    Trying to create div

--- countdown/src/Main.js
+++ countdown/src/Main.js
@@ -6,16 +6,18 @@ class Main extends Component{
 		this.state = { div: 0}
 	}
 	render(){
+		const t = this.state;
 		function createDiv(){
-			<div id={this.state.div}>
-			<p> {document.getElementById('input').value} </p>
-			<input type='button' value='X'/>
-			</div>
+			console.log(t.div);
+			// <div id={t.div}>
+				// <p> {document.getElementById('input').value} </p>
+				// <input type='button' value='X'/>
+			// </div>
 		}
 		return(
 			<div>
 				<input type='text' placeholder='new item' id='input'/>
-				<input type='button' value='+' id='button' onClick={() => createDiv}/>
+				<input type='button' value='+' id='button' onClick={() => createDiv()}/>
 			</div>
 		);
 	}
