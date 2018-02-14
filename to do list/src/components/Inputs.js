fd5eb2e98e2be728d2d911dc39dc2c2a98b9ffdb
Toprak Koç <toprakademkoc360@gmail.com>
2018-02-14 16:13:28 +0300

    to do list go back button update

--- to do list/src/components/Inputs.js
+++ to do list/src/components/Inputs.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 
 class Inputs extends Component{
   addElements(){
+    const text = this.props.page.charAt(0).toUpperCase() + this.props.page.slice(1);
     return (
       <div>
         <input id='input' placeholder='To Do' />
         <input type='button' value='Add' onClick={() => {
           this.props.addHandler(document.getElementById('input').value);
         }} />
-        <input type='button' value={this.props.page} onClick={this.props.pageHandler} />
+        <input type='button' value={text} onClick={this.props.pageHandler} />
       </div>
     );
   }
   goBackElement() {
+    const text = this.props.page.charAt(0).toUpperCase() + this.props.page.slice(1);
     return (
-      <input type='button' value={this.props.page} onClick={this.props.pageHandler} />
+      <input type='button' value={text} onClick={this.props.pageHandler} />
     );
   }
   render(){
